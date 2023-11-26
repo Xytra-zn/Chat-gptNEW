619b4b999da58c85cdb512dbfc22fa2f09d5fd86
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-26 23:48:53 +0530

    Update start.py

--- XYTRA/start.py
+++ XYTRA/start.py
@@ -7,8 +7,7 @@ bot = Client("my_bot", bot_token="6526084223:AAHRX5cIwWCyMN1vqmseXQXl1kNFFulevbs
 async def welcome_dm(client, message):
     welcome_msg = (
         "🌟 Hello there! Welcome to the Sasta Coder Bot! 🚀\n"
-        "Feel free to use the available commands or ask for help. If you have any questions or need assistance, just let me know!
-        Sasata codee=https://t.me/sasta_coder"
+        "Feel free to use the available commands or ask for help. If you have any questions or need assistance, just let me know!"
     )
 
     await message.reply_text(welcome_msg, disable_web_page_preview=True)
