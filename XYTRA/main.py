5007f44be6e87f081ac286535c609c25acd29e98
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-26 15:30:58 +0530

    Update main.py

--- XYTRA/main.py
+++ XYTRA/main.py
@@ -6,8 +6,10 @@ from XYTRA.exit import exit_msgs
 async def welcome_new_members(client, message):
     chat_id = message.chat.id
     for user in message.new_chat_members:
+        # Check if the new member has a username
+        username = user.username or "New Member"
         # Generate a random welcome message
-        welcome_msg = random.choice(welcome_msgs).format(username=user.username)
+        welcome_msg = random.choice(welcome_msgs).format(username=username)
         # Send the welcome message to the group
         await bot.send_message(chat_id, welcome_msg)
 
@@ -15,8 +17,9 @@ async def welcome_new_members(client, message):
 async def send_exit_message(client, message):
     chat_id = message.chat.id
     user = message.left_chat_member
+    # Check if the leaving member has a username
+    username = user.username or "Former Member"
     # Generate a random exit message
-    exit_msg = random.choice(exit_msgs).format(username=user.username)
+    exit_msg = random.choice(exit_msgs).format(username=username)
     # Send the exit message to the group
     await bot.send_message(chat_id, exit_msg)
-  
