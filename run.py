db8d1aa0d34bd5b68f2ccf0e7b398be839e3bb04
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-27 01:14:39 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,7 @@
+# run.py
+
+from XYTRA import main, start
+
+if __name__ == "__main__":
+    main.main()  # Assuming you have a main() function in main.py
+    start.start()  # Assuming you have a start() function in start.py
