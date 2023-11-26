51fa0250db61785189ead074fd81374525e04693
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-27 01:01:33 +0530

    Update start.py

--- XYTRA/start.py
+++ XYTRA/start.py
@@ -1,8 +1,14 @@
 from pyrogram import Client, filters
 
 # Initialize the Pyrogram client
-bot = Client("my_bot", bot_token="6526084223:AAHRX5cIwWCyMN1vqmseXQXl1kNFFulevbs", api_id="6435225", api_hash="4e984ea35f854762dcde906dce426c2d")
+bot = Client(
+    "my_bot",
+    bot_token="6526084223:AAHRX5cIwWCyMN1vqmseXQXl1kNFFulevbs",
+    api_id="6435225",
+    api_hash="4e984ea35f854762dcde906dce426c2d"
+)
 
+# Define the /start command
 @bot.on_message(filters.private & filters.command("start"))
 async def welcome_dm(client, message):
     user_id = message.from_user.id
@@ -13,19 +19,6 @@ async def welcome_dm(client, message):
 
     await message.reply_text(welcome_msg, disable_web_page_preview=True)
 
-
-@bot.on_message(filters.private & filters.command("help"))
-async def help_command(client, message):
-    # Help message with features
-    help_msg = (
-        "🚀 **Available Commands:**\n"
-        "/start - Get a welcome message\n"
-        "/help - Get information about available commands and how to use them\n\n"
-        "💡 **Bot Features:**\n"
-        "1. Sends a welcome message when you use /start\n"
-        "2. Sends a leave message when someone exits the chat\n"
-        "3. Responds to /help to provide information about available commands and features"
-    )
-
-    await message.reply_text(help_msg, disable_web_page_preview=True)
-    
+# Start the bot
+if __name__ == "__main__":
+    bot.run()
