1edefa97b47d7d2e1b2140f8dc80af87bc3a9ec6
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-20 11:31:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# main.py
+
+import os
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext
+from modules import start_module, help_module, message_module
+from config import TOKEN
+
+def main() -> None:
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    # Add command handlers from modules
+    dispatcher.add_handler(CommandHandler("start", start_module.start))
+    dispatcher.add_handler(CommandHandler("help", help_module.help_command))
+
+    # Add message handler from message module
+    dispatcher.add_handler(MessageHandler(Filters.all, message_module.process_message))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+  
