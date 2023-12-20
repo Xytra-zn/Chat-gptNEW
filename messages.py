25dc55b68b27f25a59c7377d75de216485d742a1
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-20 11:34:35 +0530

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,30 @@
+# messages.py
+
+WELCOME_MESSAGE = (
+    "👋 **Welcome to ChatGPT Bot!**\n\n"
+    "This bot utilizes Chat GPT-4 API for advanced conversational capabilities.\n\n"
+    "🚀 **Features:**\n"
+    "1. **Text Generation:**\n"
+    "   - Copywriting and rewriting\n"
+    "   - Code writing and editing\n"
+    "2. **Voice Interaction:**\n"
+    "   - Send voice messages for text generation\n"
+    "   - Conversational responses to your voice queries\n"
+    "🔗 **Stay Connected:**\n"
+    "   - Updates: [@CODERS_ZONE]\n\n"
+    "We're here to make your text interactions and voice queries seamless. "
+    "Feel free to explore the commands and enjoy the power of ChatGPT-4!\n\n"
+    "❤️ **Enjoy your ChatGPT experience!**"
+)
+
+HELP_MESSAGE = (
+    "This bot can assist you with text generation and voice interaction using Chat GPT-4 API. "
+    "Explore the available commands and enjoy the experience!\n\n"
+    "💡 **Commands:**\n"
+    "/start - Get a warm welcome and an overview of the bot\n"
+    "/help - View this help message\n\n"
+    "🎤 **Voice Interaction:**\n"
+    "Send voice messages to generate text or receive conversational responses.\n\n"
+    "🔗 **Stay Connected:**\n"
+    "Updates: [@CODERS_ZONE]"
+)
