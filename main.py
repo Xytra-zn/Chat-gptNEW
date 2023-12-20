cfae4c45b44da2acacf1d7af493ceb73b1b83a6b
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-20 22:39:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import os
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackContext
-from modules import start, help_module, message_module
+from XYTRA import start, help_module, message_module
 from config import TOKEN
 
 def main() -> None:
