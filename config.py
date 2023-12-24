62a62e8d38ade21413e84a5f6a0bbee45b5bebda
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-12-24 13:29:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-# config.py
-
 import os
 
-TOKEN = os.environ.get('TOKEN', 'your_default_telegram_token')
+TOKEN = os.environ.get('TOKEN', '6442598069:AAHQdHvE6bLQPGi1mfCKrXvIlv2MD-nat7A')
+API_ID = "6435225"
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
 GPT_API_URL = os.environ.get('GPT_API_URL', 'https://chatgpt.apinepdev.workers.dev')
